7eb675fa171c9a3996af6de04a4ac802ef99cc34
KittenKicker <oKittenKickero@gmail.com>
2015-03-20 09:07:53 -0400

    updated client and server

--- prog7/server.c
+++ prog7/server.c
@@ -1,55 +1,99 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void error(const char *msg)
+void doprocessing (int sock)
 {
-    perror(msg);
-    exit(1);
+   int n;
+   char buffer[256];
+   
+   bzero(buffer,256);
+   
+   n = read(sock,buffer,255);
+   
+   if (n < 0)
+      {
+      perror("ERROR reading from socket");
+      exit(1);
+      }
+   
+   printf("Here is the message: %s\n",buffer);
+   n = write(sock,"I got your message",18);
+   
+   if (n < 0)
+      {
+      perror("ERROR writing to socket");
+      exit(1);
+      }
 }
 
-int main(int argc, char *argv[])
+int main( int argc, char *argv[] )
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
+   int sockfd, newsockfd, portno, clilen;
+   char buffer[256];
+   struct sockaddr_in serv_addr, cli_addr;
+   int  n;
+   
+   /* First call to socket() function */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0)
+      {
+      perror("ERROR opening socket");
+      exit(1);
+      }
+   
+   /* Initialize socket structure */
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   portno = 5001;
+   
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+   
+   /* Now bind the host address using bind() call.*/
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+      {
+      perror("ERROR on binding");
+      exit(1);
+      }
+   
+   /* Now start listening for the clients, here
+   * process will go in sleep mode and will wait
+   * for the incoming connection
+   */
+   
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+   
+   while (1)
+   {
+      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+      if (newsockfd < 0)
+         {
+         perror("ERROR on accept");
          exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+         }
+      
+      /* Create child process */
+      pid = fork();
+      if (pid < 0)
+         {
+         perror("ERROR on fork");
+         exit(1);
+         }
+      
+      if (pid == 0)
+         {
+         /* This is the client process */
+         close(sockfd);
+         doprocessing(newsockfd);
+         exit(0);
+         }
+      else
+         {
+         close(newsockfd);
+         }
+   } /* end of while */
 }
