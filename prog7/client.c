3ad2258468f5949fb2e95da95e3d7378f096a58a
KittenKicker <devinh1994@gmail.com>
2015-03-21 21:29:15 -0400

    updated client and server. left to add is disconncting winners

--- prog7/client.c
+++ prog7/client.c
@@ -1,47 +1,228 @@
+/***********************************************/
+/* Devin Holland                                             
+/* Login ID: holl4332                               
+/* CS-202, Winter 2015                          
+/* Programming Assignment 7                  
+/* client for playing games of sine herd pig                    
+/***********************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <string.h>
+
+/***********************************************/
+/* Function: connect_to_server                                          
+/* Purpose: make connection to the socket                        
+/* Parameters: name of host and port number                    
+/* Returns: int socket
+/*********************************************/
 int connect_to_server(char *host, int portnum) 
 {
-    int sock;
-    struct sockaddr_in servadd;
-    struct hostent *hp;
+    int sock; //the socket
+
+    struct sockaddr_in servadd; //server address
+    struct hostent *hp; //host
+
+    //open socket
     sock = socket( AF_INET, SOCK_STREAM, 0 );
     if ( sock == -1 ) 
     {
         return -1;
     }
+
+    //zero out server address
     bzero( &servadd, sizeof(servadd) );
+
+    //get hp from host name
     hp = gethostbyname( host );
     if (hp == NULL)
     { 
         return -1;
     }
+
+    //set up server address info
     bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr,
     hp->h_length);
     servadd.sin_port = htons(portnum);
     servadd.sin_family = AF_INET ;
+
+    //connect to the socket
     if ( connect(sock,(struct sockaddr *)&servadd,
         sizeof(servadd)) !=0) 
     {
         return -1;
     }
+
     return sock;
 }
 
-talk_with_server(int fd) 
+/***********************************************/
+/* Function: getMove                                         
+/* Purpose: get players move of move                         
+/* Parameters:none                      
+/* Returns: int move number          
+/*********************************************/
+int getMove() 
 {
-    char buf[LEN];
-    int n;
-    n = read(fd, buf, LEN);
-    write(1, buf, LEN);
+  printf("sit or stand?\n"); //ask user move
+  char move[16]; //read move
+  int index = 0; //index for making word lowercase
+
+  fgets(move, sizeof(move), stdin); // get move
+
+  //make move input lowercase
+  for(index = 0; move[index]; index++) 
+  {
+    move[index] =  tolower(move[index]);
+  }
+
+  //check if they typed hold or sit
+  if((strncmp("hold", move, strlen("hold")) == 0) || (strncmp("sit", move, strlen("sit")) == 0)) 
+  {
+    printf("(will wait for other player to finish round if necessary)\n");
+    return 0;
+  } 
+  //check if they rolled or stand
+  else if((strncmp("roll", move, strlen("roll")) == 0) || (strncmp("stand", move, strlen("stand")) == 0)) 
+  {
+    return 1;
+  } 
+  else //they input something dumb
+  {
+    printf("Please pick sit or stand\n");
+    memset(move, 0, sizeof(move));
+    return -1;
+  }
 }
 
-main(int argv, char *argc[]) 
+/***********************************************/
+/* Function: playGame                                          
+/* Purpose: handles input and output for playing game                        
+/* Parameters:int file descriptor for client                     
+/* Returns: none           
+/*********************************************/
+playGame(int fd)
 {
-    int fd;
-    fd = connect_to_server(argc[1], 5123);
-    if (fd == -1) 
+    char buf[1024]; //input from server
+    char userMove[256]; //user's move choice
+    int nbytes; //number of bytes read
+    int gameOver = 0; //is the game over
+    int move; //move of player
+
+    //read from server
+    nbytes = recv(fd, buf, sizeof(buf),0);
+    buf[nbytes] = '\0';
+
+    //first read is opponents username
+    printf("Your opponent is: %s\n", buf);
+    //send confirmation
+    write(fd, "good", 4);
+    //clear buffer
+    memset(buf, 0, sizeof(buf));
+
+    while(!gameOver) //game is still going
+    {
+        //read next input
+        nbytes = read(fd, buf, sizeof(buf));
+        buf[nbytes] = '\0';
+
+        //roll announcement
+        if(strncmp("Roll was", buf, strlen("Roll was")) == 0)
+        {
+            //print roll and confirm
+            printf("%s\n", buf);
+            write(fd, "good", 4);
+        }
+        // opponent move announcement
+        else if(strncmp("Opponent's", buf, strlen("Opponent's"))==0)
+        {
+            //print move and confirm
+            printf("%s\n", buf);
+            write(fd, "good", 4);
+        }
+        //score update
+        else if(strncmp("Player 1 Score", buf, strlen("Player 1 Score")) == 0)
+        {
+            //print score and confirm
+            printf("%s\n", buf);
+            write(fd, "good", 4);
+        }
+        //get move request
+        else if(atoi(buf) == 777)
+        {
+            do //prompt user till valid response
+            {
+                move = getMove();
+            } while(move == -1);
+
+            //write move to server
+            sprintf(userMove, "%d", move);
+            write(fd, userMove, sizeof(move));
+
+            //clear move buffer
+            memset(userMove, 0 , sizeof(move));
+        }
+        //game is over
+        else if(strncmp("Game over", buf, strlen("Game over")) == 0)
+        {
+            //print winner and confirm
+            printf("%s\n",  buf);
+            write(fd, "good", 4);
+
+            //set game over flag
+            gameOver = 1;
+        }
+
+        //clear buffer
+        memset(buf, 0, sizeof(buf));
+    }
+    return;
+}
+
+/***********************************************/
+/* Function: sendUsername                                          
+/* Purpose: write username to serever                        
+/* Parameters:int file descriptor and username                    
+/* Returns: none           
+/*********************************************/
+sendUsername(int fd, char username[256])
+{
+    //write username to server and start playing
+    write(fd, username, strlen(username));
+    playGame(fd);
+}
+
+/***********************************************/
+/* Function: process_reqeust                                          
+/* Purpose: handle starting up and playing game                         
+/* Parameters:int file descriptor for each player                     
+/* Returns: none           
+/*********************************************/
+int main(int argv, char *argc[]) 
+{
+    int fd; //file descriptor for client
+    char username[256]; //inpt username
+    
+    //connect to server
+    fd = connect_to_server("127.0.0.1", 5123);
+
+    if(fd == -1) //couldn't connect
     {
         exit(1);
     }
-    talk_with_server(fd);
+    //prompt for username
+    printf("Enter your username\n");
+    fgets(username, 256, stdin);
+  
+    //send username and start playing
+    sendUsername(fd, username);
     close (fd);
+
+    return 0;
 }
--- prog7/server.c
+++ prog7/server.c
@@ -1,3 +1,10 @@
+/***********************************************/
+/* Devin Holland                                             
+/* Login ID: holl4332                               
+/* CS-202, Winter 2015                          
+/* Programming Assignment 7                   
+/* Server for running games of swine herd pig                    
+/***********************************************/
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -6,70 +13,478 @@
 #include <netdb.h>
 #include <time.h>
 #include <strings.h>
+#include <string.h>
+#include <stdlib.h>
 #define HOSTLEN 256
 #define BACKLOG 1
+
+/***********************************************/
+/* Function: make_server_socket_q (defined later)               
+/***********************************************/
 int make_server_socket_q(int , int );
 
+/***********************************************/
+/* Function: make_server_socket                                           
+/* Purpose: take port and backlog & cal
+/*     make server socket q                              
+/* Parameters: int portnum                        
+/* Returns: int socket id                  
+/*********************************************/
 int make_server_socket(int portnum) 
 {
  return make_server_socket_q(portnum, BACKLOG);
 }
 
+/***********************************************/
+/* Function: make_server_socket_q                                           
+/* Purpose: take port and backlog & create socket                           
+/* Parameters: int portnum, int backlog                      
+/* Returns: int socket id                  
+/*********************************************/
 int make_server_socket_q(int portnum, int backlog) 
 {
-    struct sockaddr_in saddr; 
-    struct hostent *hp;
-    char hostname[HOSTLEN]; 
-    int sock_id;
+    struct sockaddr_in saddr; //socket address
+    struct hostent *hp; //host
+    char hostname[HOSTLEN];  //name of host
+    int sock_id; //id of socket created
+
+    //creae socket and check that it worked
     sock_id = socket(PF_INET, SOCK_STREAM, 0);
     if ( sock_id == -1 ) 
     {
         return -1;
     }
+
+    //zero out socket address
     bzero((void *)&saddr, sizeof(saddr));
-    // gethostname(hostname, HOSTLEN); // still bogus
-    // hp = gethostbyname(hostname);
-    // bcopy( (void *)hp->h_addr, (void *)&saddr.sin_addr,
-    // hp->h_length);
-    saddr.sin_addr.s_addr = INADDR_ANY; // NOTE FIX!!!!
+
+    //set socket address port and family
+    saddr.sin_addr.s_addr = INADDR_ANY;
     saddr.sin_port = htons(portnum);
     saddr.sin_family = AF_INET ;
+
+    //bind info to socket id
     if ( bind(sock_id, (struct sockaddr *)&saddr,
         sizeof(saddr)) != 0 ) 
     {
         return -1;
     }
+
+    //set up listener on socket
     if ( listen(sock_id, backlog) != 0 ) 
     {
         return -1;
     }
+
     return sock_id;
 }
 
-process_request(int fd) 
+/***********************************************/
+/* Function: getRandomNumber                                           
+/* Purpose: generate random number 1-6                              
+/* Parameters: none                       
+/* Returns: int randomized number                 
+/*********************************************/
+int getRandomNumber() 
 {
-    time_t now;
-    char *cp;
-    time(&now);
-    cp = ctime(&now);
-    write(fd, cp,
-    strlen(cp));
+  int randomNumber; //the number
+  //get 1-6
+  randomNumber = (rand() % 6) + 1;
+
+  return randomNumber;
 }
 
-main() 
+/***********************************************/
+/* Function: isGameOver                                          
+/* Purpose: tell if anyone has won and if so who                              
+/* Parameters: int first players score, second players score                       
+/* Returns: int who won   (-1 if not over)                
+/*********************************************/
+int isGameOver(int p1Points, int p2Points) 
 {
-    int sock;
-    int fd;
-    sock = make_server_socket(5123);
-    if (sock == -1) 
+  if(p1Points >= 100 && p1Points > p2Points)
+  {
+    return 0; //player 1 won
+  }
+  else if(p2Points >= 100 && p2Points > p1Points)
+  {
+    return 1; //player 2 won
+  }
+  else if(p2Points >= 100 && p2Points == p1Points)
+  {
+    return 2; //tie game
+  }
+  else
+  {
+    return -1; //games not over
+  }
+}
+
+/***********************************************/
+/* Function: sendTotalScore                                          
+/* Purpose: send scores to client                          
+/* Parameters: int player 1 poitns, player 2 points, 
+/*             file descriptor                      
+/* Returns: int socket id                  
+/*********************************************/
+int sendTotalScore(int p1Points, int p2Points, int fd) 
+{
+  char buffer[256]; //set up string to send
+  sprintf(buffer, "Player 1 Score: %d Player 2 Score: %d\n", p1Points, p2Points);
+
+  //send to client
+  write(fd, buffer, sizeof(buffer));
+
+  //confirm client got & printed scores
+  recv(fd, buffer, 4, 0);
+
+  return 0;
+}
+
+/***********************************************/
+/* Function: sendMove                                           
+/* Purpose: send info if their opponent is still standing                           
+/* Parameters:int file descriptor                      
+/* Returns: int                
+/*********************************************/
+int sendMove(int fd)
+{
+    char buffer[256]; //set up striing to send
+    //the opponent is still standing and hasnt sat yet
+    sprintf(buffer, "Opponent's choice: Still Standing");
+
+    //send to client
+    write(fd, buffer, sizeof(buffer));
+
+    //wait for confirmation from client
+    recv(fd, buffer, 4, 0);
+    return 0;
+}
+
+/***********************************************/
+/* Function: sendRoll                                          
+/* Purpose: tell clients what the roll was                           
+/* Parameters:int roll,  file descriptor                      
+/* Returns: int                
+/*********************************************/
+int sendRoll(int roll, int fd) 
+{
+  char buffer[256]; //hold string to send
+
+  sprintf(buffer, "Roll was: %d\n", roll);
+
+  //send roll to client
+  write(fd, buffer, sizeof(buffer));
+
+  //wait for confirmation
+  recv(fd, buffer, 4,0);
+  return 0;
+}
+
+/***********************************************/
+/* Function: getMove                                           
+/* Purpose: send request to client for move                          
+/* Parameters:int file descriptor                  
+/* Returns: int     chosen move           
+/*********************************************/
+int getMove(int fd) 
+{
+  //ftruncate(fd,0);
+  char buffer[256]; //hold signal to send
+  int getMoveSignal = 777; 
+  // 777 protocool for request
+
+  int nbytes; //number of bytes receied
+
+  sprintf(buffer, "%d", getMoveSignal);
+
+  //send request to client
+  write(fd, buffer, sizeof(buffer));
+
+  //clear buffer and wait for response
+  memset(buffer, 0, sizeof(buffer));
+  nbytes = read(fd, buffer, sizeof(buffer));
+
+  //null terminate buffer
+  buffer[nbytes] = '\0';
+
+  //returns the number chosen for move
+  return atoi(buffer);
+}
+
+/***********************************************/
+/* Function: playGame                                          
+/* Purpose: run the game                          
+/* Parameters: file descriptors for player 1 and 2                      
+/* Returns: int                
+/*********************************************/
+int playGame(int fdp1, int fdp2) 
+{
+    //set up total and round scoreas
+  int p1TotalScore = 0;
+  int p2TotalScore = 0;
+  int p1RoundScore = 0;
+  int p2RoundScore = 0;
+
+  //set moves to stand
+  int p1Move = 1;
+  int p2Move = 1;
+
+  //the die roll
+  int roll = 0;
+
+  //is game over?
+  int gameOverResult = -1;
+
+  //while the game is not over
+  while(gameOverResult == -1) 
+  {
+    //get a new roll and send to users
+    roll = getRandomNumber();
+    sendRoll(roll, fdp1);
+    sendRoll(roll, fdp2);
+
+    //if roll is 1 set player round score to 0 if they were
+    //standing, otherwise add to their total score
+    if(roll == 1) 
     {
+      if(p1Move == 1) 
+      {
+        p1RoundScore = 0;
+      }
+      if(p2Move == 1) 
+      {
+        p2RoundScore = 0;
+      }
+      p1TotalScore = p1TotalScore + p1RoundScore;
+      p2TotalScore = p2TotalScore + p2RoundScore;
+
+      //reset round score
+      p1RoundScore = 0;
+      p2RoundScore = 0;
+
+      //send total score update
+      sendTotalScore(p1TotalScore, p2TotalScore, fdp1);
+      sendTotalScore(p1TotalScore, p2TotalScore, fdp2);
+
+      //reset move to stand
+      p1Move = 1;
+      p2Move = 1;
+
+      //see if game is over
+      gameOverResult = isGameOver(p1TotalScore, p2TotalScore);
+    } 
+    else  //roll is not 1
+    {
+      //if the player is standing add points to their round score
+      if(p1Move == 1) 
+      {
+        p1RoundScore = p1RoundScore + roll;
+      }
+      if(p2Move == 1) 
+      {
+        p2RoundScore = p2RoundScore + roll;
+      }
+    }
+
+    //if game isn't over and the roll wasn't 1
+    if(gameOverResult == -1 && roll != 1) 
+    {
+        //if player isn't sitting, ask for next move
+        if(p1Move != 0)
+        {
+            p1Move = getMove(fdp1);
+        }
+        if(p2Move != 0)
+        {
+            p2Move = getMove(fdp2);
+        }      
+
+        //let player 2 know player 1 is still standing
+        if(p1Move != 0)
+        {
+            sendMove(fdp2);
+        }
+
+        //let player 1 know player 2 is still standing
+        if(p2Move != 0)
+        {
+            sendMove(fdp1);
+        }
+
+        //if both players sat
+        if(p1Move == 0 && p2Move == 0) 
+        {
+            //add to total score
+            p1TotalScore = p1TotalScore + p1RoundScore;
+            p2TotalScore = p2TotalScore + p2RoundScore;
+
+            //reset round score and send score updates
+            p1RoundScore = 0;
+            p2RoundScore = 0;
+            sendTotalScore(p1TotalScore, p2TotalScore, fdp1);
+            sendTotalScore(p1TotalScore, p2TotalScore, fdp2);
+
+            //reset players to standing
+            p1Move = 1;
+            p2Move = 1;
+
+            //check if game is over
+            gameOverResult = isGameOver(p1TotalScore, p2TotalScore);
+      }
+    }
+  }
+  return gameOverResult; //who won/tie
+}
+
+/***********************************************/
+/* Function: process_reqeust                                          
+/* Purpose: handle starting up and playing game                         
+/* Parameters:int file descriptor for each player                     
+/* Returns: none           
+/*********************************************/
+process_request(int fd, int fd2) 
+{
+    //response from client
+    char response[5];
+
+    //username lengths
+    int username1Length;
+    int username2Length;
+
+    //usernames
+    char username[256];
+    char username2[256];
+
+    //buffer & clearing
+    char buf[256];
+    memset(buf, 0, 256);
+
+    //result of game
+    int result;
+
+    //read in usernames
+    username1Length = recv(fd, username, sizeof(username), 0);
+    username2Length = recv(fd2, username2, sizeof(username2), 0);
+    
+    //null terminate usernames
+    username[username1Length] = '\0';
+    username2[username2Length] = '\0';
+
+    //print opponent to clients
+    printf("Connected: %s\n", username);
+    write(fd2, username, strlen(username));
+    printf("Connected: %s\n", username2);
+    write(fd, username2, strlen(username2));
+
+    //wait for confirmation from clients
+    recv(fd2, response, 4, 0);
+    recv(fd, response, 4, 0);
+
+    //start the game and wait for who won
+    result = playGame(fd, fd2);
+
+    //player 1 won
+    if(result == 0)
+    {
+        //tell clients who won
+        sprintf(buf, "Game over, %s won the game.", username);
+        write(fd, buf, strlen(buf));
+        write(fd2, buf, strlen(buf));
+
+        //wait for confirmation
+        recv(fd, response, 4, 0);
+        recv(fd2, response, 4, 0);
+
+        //close file descriptors and exit
+        close(fd);
+        close(fd2);
+        exit(0);
+    }
+    //player 2 won
+    else if( result == 1)
+    {
+        //tell clients who won
+        sprintf(buf, "Game over, %s won the game.", username2);
+        write(fd, buf, strlen(buf));
+        write(fd2, buf, strlen(buf));
+
+        //wait for confirmation
+        recv(fd, response, 4, 0);
+        recv(fd2, response, 4, 0);
+
+        //close file descriptors and exit
+        close(fd);
+        close(fd2);
+        exit(0);
+    }
+    //tie
+    else if(result == 2)
+    {
+        //tell clients it's a tie
+        sprintf(buf, "Game over, It's a tie game.");
+        write(fd, buf, strlen(buf));
+        write(fd2, buf, strlen(buf));
+
+        //wait for confirmation
+        recv(fd, response, 4, 0);
+        recv(fd2, response, 4, 0);
+
+        //close file descriptors and exit
+        close(fd);
+        close(fd2);
+        exit(0);
+    }
+    return;
+}
+
+/***********************************************/
+/* Function: main                                           
+/* Purpose: create child processes to play games in                         
+/* Parameters:none                     
+/* Returns: int                
+/*********************************************/
+int main() 
+{
+    int sock; //socket id
+
+    sock = make_server_socket(5123); //create socket
+    if (sock == -1) 
+    { //check that it worked
+        perror("Error opening socket");
         exit(1);
     }
-    while(1) 
+    while(1) //wait for multiple games
     {
+        srand(time(NULL)); //set up random generator
+        
+        int fd; //file descriptor 1
+        int fd2; // file descriptor 2
+        int pid; //process id
+
+        //accept sockets
         fd = accept(sock, NULL, NULL);
-        if ( fd == -1) break;
-        process_request(fd);
-        close(fd);
+        if ( fd == -1) 
+        {
+            return;
+        }
+        fd2 = accept(sock, NULL, NULL);
+        if (fd2 == -1) 
+        {
+            return;
+        }
+
+        pid = fork(); //fork for a new game
+        //play the game
+        if(pid == 0)
+        {
+            process_request(fd, fd2);
+        }
+        else if(pid == -1)
+        {
+            perror("couldn't fork");
+        }
     }
+    
+    return 0;
 } 
